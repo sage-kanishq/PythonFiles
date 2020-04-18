e596b18f87d057cd76830c0256f2d74ead19bb6c
KanishqSoccer <kanishq.sarkar@gmail.com>
2020-04-18 20:18:47 +0530

    Testrail Ready

--- TestRailAPI/testrail_api.py
+++ TestRailAPI/testrail_api.py
@@ -1,6 +1,6 @@
 from pprint import pprint
-
-from testrail import *
+import json
+from testrail import APIClient,APIError
 
 import requests
 
@@ -8,45 +8,62 @@ client = APIClient('')
 client.user = ''
 client.password = ''
 
+class TestRailClient:
+    def add_result(self,test_id,status:str):
+        _ = client.send_post(f'add_result/{test_id}',{"status_id":5})
+    def add_result_for_case(self,run_id: int, case_id: int, status: str, msg: str):
+        """Add test case and update result
 
-def add_result_for_case(*, run_id: int, case_id: int, status: str, msg: str):
-    """Add test case and update result
+        """
+        if status.lower() == "pass":
+            result = client.send_post(
+                'add_result_for_case/%d/%d' % (run_id, case_id),
+                {'status_id': 1, 'comment': msg}
+            )
+            return result
+        elif status.lower() == "fail":
+            result = client.send_post(
+                'add_result_for_case/%d/%d' % (run_id, case_id),
+                {'status_id': 5, 'comment': msg}
+            )
+            return result
+        elif status.lower() == "block":
+            result = client.send_post(
+                'add_result_for_case/%d/%d' % (run_id, case_id),
+                {'status_id': 3, 'comment': msg}
+            )
+            return result
+        elif status.lower() == "retest":
+            result = client.send_post(
+                'add_result_for_case/%d/%d' % (run_id, case_id),
+                {'status_id': 4, 'comment': msg}
+            )
+            return result
 
-    """
-    if status.lower() == "pass":
-        result = client.send_post(
-            'add_result_for_case/%d/%d' % (run_id, case_id),
-            {'status_id': 1, 'comment': msg}
-        )
+        else:
+            raise Exception("Not a valid status")
+
+
+    def get_run(self,run_id: int):
+        run = client.send_get("get_run/%d" % (run_id))
+        return run
+
+    def get_case(self,case_id: int):
+        case = client.send_get("get_case/%d" % (case_id))
+        return case
+
+
+    def get_results(self,test_id):
+        results = client.send_get("get_results/%d"%(test_id))
+        
+        return results
+
+    def get_results_for_case(self,run_id,case_id):
+        result = client.send_get(f"get_results_for_case/{run_id}/{case_id}")
         return result
-    elif staus.lower() == "fail":
-        result = client.send_post(
-            'add_result_for_case/%d/%d' % (run_id, case_id),
-            {'status_id': 5, 'comment': msg}
-        )
-        return result
-    elif status.lower() == "block":
-        result = client.send_post(
-            'add_result_for_case/%d/%d' % (run_id, case_id),
-            {'status_id': 3, 'comment': msg}
-        )
-        return result
-    elif status.lower() == "retest":
-        result = client.send_post(
-            'add_result_for_case/%d/%d' % (run_id, case_id),
-            {'status_id': 4, 'comment': msg}
-        )
-        return result
-
-    else:
-        raise Exception("Not a valid status")
 
 
-def get_run(run_id: int):
-    run = client.send_get("get_run/%d" % (run_id))
-    return run
+
+a = TestRailClient()
 
 
-def get_case(case_id: int):
-    case = client.send_get("get_case/%d" % (case_id))
-    return case
